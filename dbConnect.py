c9297959617b1443879dd49156e809a561021f06
Emin Mastizada <emin25@ovi.com>
2014-10-20 17:39:06 +0300

    Database connection module version 2014-10-20

---
+++ dbConnect.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__name__ = "dbConnect"
+__description__ = 'Database Connection Module'
+__autor__ = "Emin Mastizada"
+__version__ =  '2014-10-20'
+
+import mysql.connector	# MySQL Connector
+from mysql.connector import errorcode
+import yaml
+settings = yaml.load(open("credentials.yml", 'r'))
+### Settings file can be checked for keys:
+def check_settings():
+	if not 'db' in settings.keys():
+		return False
+	keys = ['user','password','database']
+	return all(key in settings['db'].keys() for key in keys)
+
+def connect():
+	"""
+	Creates connection to database, returns Connection or boolean False
+	"""
+	if not check_settings():
+		return False
+	try:
+		con = mysql.connector.connect(
+			user=settings['db']['user'],
+			password=settings['db']['password'],
+			host=settings['db']['host'],
+			database=settings['db']['database'],
+			charset='utf8')
+	except mysql.connector.Error as err:
+		if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+			print("Wrong credentials, ACCESS DENIED")
+		elif err.errno == errorcode.ER_BAD_DB_ERROR:
+			print("Database %s does not exists" % (settings['db']['database']))
+		else:
+			print(err) # Change this code, it can be written to log file or just printed.
+		return False
+	return con
+
+def disconnect(con):
+	"""
+	Disconnect from db
+	"""
+	con.close()
