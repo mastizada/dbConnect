9e6bbe6ec5dd345221216c97b2daa3b7a42030e4
Said <saidozcn@gmail.com>
2016-01-27 22:41:00 +0200

    updates setup.py to reformat

--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
 from setuptools import setup
 
-setup(name='dbConnect',
-      version='1.4.6',
-      description='MySQL for Humans',
-      keywords='dbConnect mysql simple easy, light connection module',
-      url='https://github.com/mastizada/dbConnect',
-      author='Emin Mastizada',
-      author_email='emin@linux.com',
-      license='MPLv2',
-      packages=['dbConnect'],
-      install_requires=[
-          'setuptools',
-      ],
-      classifiers=[
+setup(
+    name='dbConnect',
+    version='1.4.6',
+    description='MySQL for Humans',
+    keywords='dbConnect mysql simple easy, light connection module',
+    url='https://github.com/mastizada/dbConnect',
+    author='Emin Mastizada',
+    author_email='emin@linux.com',
+    license='MPLv2',
+    packages=['dbConnect'],
+    install_requires=[
+        'setuptools',
+    ],
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Database :: Front-Ends',
         'Environment :: Plugins',
-      ],
-      scripts=['dbConnect/dbConnect.py'],
-      zip_safe=False)
+    ],
+    scripts=['dbConnect/dbConnect.py'],
+    zip_safe=False
+)
