ed7a949c690b6363ba93ff6b4727fcc0556b2f8b
Emin Mastizada <emin@mastizada.com>
2016-06-06 04:50:03 +0300

    Setup updated with long readme and python versions for pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup
 
+with open('README.rst', 'r', 'utf-8') as f:
+    readme = f.read()
+
 setup(
     name='dbConnect',
     version='1.5.0',
     description='MySQL for Humans',
+    long_description=readme,
     keywords='dbConnect mysql simple easy, light connection module',
     url='https://github.com/mastizada/dbConnect',
     author='Emin Mastizada',
@@ -17,7 +21,17 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
         'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
         'Topic :: Database :: Front-Ends',
         'Environment :: Plugins',
     ],
